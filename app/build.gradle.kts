80e105e42e8dcc590ea0e15f68539159a2018b58
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2025-07-05 20:47:10 +0600

    add retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // compose navigation
     implementation("androidx.navigation:navigation-compose:2.9.1")
+
+    //serialization for navigation
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
 }
